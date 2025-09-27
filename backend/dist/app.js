6c0b374fa8325165ef937a038ff4d83a1a3dc569
huu <trandaihuu4766@gmail.com>
2025-09-28 01:42:10 +0700

    fix cors

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -11,19 +11,27 @@ import authRoutes from "./routes/auth.routes.js";
 import commentRoutes from "./routes/comment.routes.js";
 import uploadRoutes from "./routes/upload.routes.js";
 import path from "path";
-const allowedOrigins = [
-  "http://localhost:3000", // FE dev
-  "http://127.0.0.1:3000",
-  "https://free-code-hub-website.vercel.app",
-  // FE production
-];
-app.use(
-  cors({
-    origin: allowedOrigins,
+// const allowedOrigins = [
+//   "http://localhost:3000", // FE dev
+//   "http://127.0.0.1:3000",
+//   "https://free-code-hub-website.vercel.app", // FE production
+// ];
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin)
+            return callback(null, true); // Cho Postman / server-to-server
+        if (origin === "http://localhost:3000" || // dev local
+            origin === "http://127.0.0.1:3000" || // dev local khác
+            origin === "https://free-code-hub-project.vercel.app/" || // FE production
+            /\.vercel\.app$/.test(origin) // mọi subdomain *.vercel.app (preview)
+        ) {
+            return callback(null, true);
+        }
+        return callback(new Error("Not allowed by CORS: " + origin));
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 app.use("/uploads", express.static(path.join(process.cwd(), "public/uploads")));
